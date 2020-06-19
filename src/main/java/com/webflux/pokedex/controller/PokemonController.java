42f65d4c3fd9183fbf56779997e35ffa122028c0
Giovana do Nascimento <giovana.np1@gmail.com>
2020-06-18 23:49:35 -0300

    frontend start

--- src/main/java/com/webflux/pokedex/controller/PokemonController.java
+++ src/main/java/com/webflux/pokedex/controller/PokemonController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.result.view.Rendering;
 
 import com.webflux.pokedex.model.Pokemon;
 import com.webflux.pokedex.model.PokemonEvent;
@@ -78,5 +79,10 @@ public class PokemonController {
 			.map(value ->
 				new PokemonEvent(value, "Pokemonsitosss"));
 	}
+	
+	@GetMapping("/")
+    public Rendering redirect() {
+        return Rendering.redirectTo("index.html").build();
+    }
 	
 }
